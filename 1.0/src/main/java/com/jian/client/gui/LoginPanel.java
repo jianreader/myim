6dadc3a52a5ecf6546e64cc0910763b46b1a32ad
patrickHPC <patrick@192.168.1.100>
2015-08-10 23:01:47 -0400

    update

--- 1.0/src/main/java/com/jian/client/gui/LoginPanel.java
+++ 1.0/src/main/java/com/jian/client/gui/LoginPanel.java
@@ -4,11 +4,15 @@ import com.jian.server.Account;
 import com.jian.server.LoginException;
 import com.jian.server.LoginOperation;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 
 /**
@@ -17,7 +21,10 @@ import java.sql.SQLException;
 public class LoginPanel extends JPanel {
     private JButton _loginButton = new JButton("Login");
     private JTextField _uiField = new JTextField(14);
+    private JLabel _uiLabel = new JLabel("User ID: ");
+    private JLabel _pwLabel = new JLabel("Password: ");
     private JPasswordField _passwordField = new JPasswordField(14);
+    private static final String LOGIN_IMAGE = "login.png";
     private Account _account ;
     private WizardController _controller;
 
@@ -30,9 +37,57 @@ public class LoginPanel extends JPanel {
 
     public void createInitGUI()
     {
-        add(_uiField, BorderLayout.PAGE_START);
-        add(_passwordField, BorderLayout.CENTER);
-        add(_loginButton, BorderLayout.PAGE_END);
+        setPreferredSize(new Dimension(200, 600));
+
+        GridBagLayout layout = new GridBagLayout();
+        setLayout(layout);
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        try {
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(LOGIN_IMAGE);
+            BufferedImage myPicture = ImageIO.read(is);
+            JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+            add(picLabel);
+            gbc.fill = GridBagConstraints.NONE;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.ipady = 8;
+            add(picLabel, gbc);
+        } catch (IOException ex) {
+            // handle exception...
+        }
+
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        add(_uiLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        add(_uiField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        add(_pwLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        add(_passwordField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 8;
+        add(new JLabel(" "), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 9;
+        add(_loginButton, gbc);
+
+        for(int i = 1 ; i < 10; ++i)
+        {
+            gbc.gridx = 0;
+            gbc.gridy = 9 + i;
+            add(new JLabel(" "), gbc);
+        }
 
         _loginButton.addActionListener(new ActionListener() {
             @Override
